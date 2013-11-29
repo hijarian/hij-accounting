65e456ecfd6623d63853c9e3eb75472373f29ca2
Saphronov Mark <hijarian@gmail.com>
2013-11-29 20:33:19 +0400

    Added code to prepare data for rendering Highcharts chart.
    TODO: implement real drawing of histogram from given data.
    TODO: implement real fetching of data from DB.

---
+++ assets/js/histogram.js
@@ -0,0 +1,3 @@
+/**
+ * Created by hijarian on 29.11.13.
+ */
--- src/ReportController.php
+++ src/ReportController.php
@@ -1,7 +1,7 @@
 <?php
 /** hijarian 27.11.13 13:21 */
 
-class ReportController
+class ReportController extends BaseController
 {
 	/**
 	 * Show histogram with expenses by type and date.
@@ -10,6 +10,37 @@ class ReportController
 	 */
 	public function histogram($f3)
 	{
-		echo "Report here";
+		$f3->set('title', 'Отчёт о тратах');
+		$f3->set('content', 'src/ui/histogram.html');
+
+		list($categories, $series) = $this->getHistogramData();
+
+		$f3->set('categories', json_encode($categories));
+		$f3->set('series', json_encode($series));
+		$this->installAssets($f3);
+		$this->render($f3);
 	}
-} 
+
+	/** @param Base $f3 */
+	private function installAssets($f3)
+	{
+		$f3->set(
+		   'jsbodyfiles',
+		   [
+			   "/assets/js/vendor/highcharts.js",
+			   "/assets/js/vendor/highcharts.themes.grid.js",
+		   ]
+		);
+	}
+
+	private function getHistogramData()
+	{
+		return [
+			[1, 2, 3],
+			[
+				[1, 2, 3],
+				[4, 5, 6]
+			]
+		];
+	}
+}
