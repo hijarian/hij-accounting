1c9b3af9d0a1132db01040175cf4470ec6419205
Saphronov Mark <hijarian@gmail.com>
2013-11-30 08:15:50 +0400

    Fixed the horrible error handling in the app.

--- index.php
+++ index.php
@@ -3,13 +3,11 @@
 
 define('ROOT_DIR', realpath(__DIR__));
 
-ini_set('display_errors', 1);
-error_reporting(-1);
-
 $f3=require(ROOT_DIR .'/lib/f3/base.php');
+
 $f3->set('AUTOLOAD', 'src/;src/datatypes/');
 $f3->set('db_path', ROOT_DIR . '/src/spending.db');
-$f3->set('DEBUG',0);
+$f3->set('DEBUG',5);
 
 /** @param Base $f3 */
 $f3->route('GET /', function ($f3) {
@@ -23,5 +21,11 @@ $f3->route('POST /add', 'SpendingController->addNew');
 $f3->route('POST /correct', 'SpendingController->correctField');
 
 $f3->route('GET /report', 'ReportController->histogram');
+
+$f3->set('ONERROR', function ($f3) {
+	var_dump($f3->get('ERROR'));
+});
+ini_set('display_errors', 1);
+error_reporting(-1);
 
 $f3->run();
